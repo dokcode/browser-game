f067b2f1839df0bf586c8721af7b7ba2d153a3d3
David Keith <keithdrd456@gmail.com>
2025-03-06 23:34:58 -0500

    updated json files

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 
 function App() {
   
+    
+  
    return (
     <>
       <div>
-        <h1>Browser Game</h1>
+        <h1 className="font-bold underline text-red-500">Browser Game</h1>
+        <button className="rounded-full bg-blue-400 px-6 py-6">Click Me</button>
       </div>
     </>
   )
